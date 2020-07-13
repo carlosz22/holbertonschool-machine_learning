d069a0e3ceff671f592a67d37eaf8fd483886af0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-07-13 15:36:42 -0500

    Add exercise 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays"""
+    result = arr1
+    result.extend(arr2)
+    return result
