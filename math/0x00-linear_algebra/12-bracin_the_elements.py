404bb286c075adfeca4e76e1e70575569374d6b2
Carlos Zuluaga <carlosez9@hotmail.com>
2020-07-14 14:35:01 -0500

    Add exercise 11 and 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" Performs element-wise addition, substraction,
+     multiplication and division"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise calculations"""
+
+    result_tuple = (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2, )
+    return result_tuple
