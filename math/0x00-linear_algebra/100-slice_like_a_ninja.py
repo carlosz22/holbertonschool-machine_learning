26fe2326f5c8a36737624eed6cb8ed13ddf5955d
Carlos Zuluaga <carlosez9@hotmail.com>
2020-07-14 15:46:47 -0500

    Add exercise 15

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Slices a matrix along a specific axes"""
+
+
+def np_slice(matrix, axes={}):
+    """Slices a matrix along a specific axes"""
+
+    max_key = max(axes.keys())
+    new_matrix = matrix.copy()
+    slice_list = []
+
+    for i in range(max_key + 1):
+        values = axes.get(i, None)
+        if values is not None:
+            slice_list.append(slice(*values))
+        else:
+            slice_list.append(slice(None))
+
+    return new_matrix[tuple(slice_list)]
