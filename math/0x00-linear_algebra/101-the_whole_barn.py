0736b9db90afe485bd7c444b3bddeab928c3e0e4
Carlos Zuluaga <carlosez9@hotmail.com>
2020-07-14 15:59:11 -0500

    Add exercise 16

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""Adds two matrices recursively"""
+
+
+def add_matrices(mat1, mat2):
+    """Adds two matrices recursively"""
+
+    new_matrix = []
+
+    if (len(mat1) != len(mat2)):
+        return None
+
+    if (type(mat1) == list and type(mat1[0]) == list):
+        for i in range(len(mat1)):
+            value = add_matrices(mat1[i], mat2[i])
+            if value is not None:
+                new_matrix.append(value)
+            else:
+                return None
+    else:
+        for i in range(len(mat1)):
+            new_matrix.append(mat1[i] + mat2[i])
+
+    return new_matrix
