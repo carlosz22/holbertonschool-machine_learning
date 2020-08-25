4376301931651eb50e4869199688216761d494ad
Carlos Zuluaga <carlosez9@hotmail.com>
2020-08-24 22:30:10 -0500

    Fix PEP8 exer 4

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -7,16 +7,16 @@ precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
-  """
-  Calculates the F1 score of a confusion matrix
+    """
+    Calculates the F1 score of a confusion matrix
     - confusion is a confusion numpy.ndarray of shape (classes, classes)
-      where row indices represent the correct labels and column indices
-      represent the predicted labels
+        where row indices represent the correct labels and column indices
+        represent the predicted labels
     - classes is the number of classes
     Returns: a numpy.ndarray of shape (classes,)
-     containing the F1 score of each class
-  """
-  sens_value = sensitivity(confusion)
-  prec_value = precision(confusion)
+        containing the F1 score of each class
+    """
+    sens_value = sensitivity(confusion)
+    prec_value = precision(confusion)
 
-  return 2 * (prec_value * sens_value) / (prec_value + sens_value)
+    return 2 * (prec_value * sens_value) / (prec_value + sens_value)
