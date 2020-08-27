6d94f662a5fe5a42aa3a07eea46c983c406a616a
Carlos Zuluaga <carlosez9@hotmail.com>
2020-08-27 15:18:55 -0500

    Add exercise 3

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -9,7 +9,8 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     Creates a Tensorflow layer that includes L2 regularization
         - prev is a tensor containing the output of the previous layer
         - n is the number of nodes the new layer should contain
-        - activation is the activation function that should be used on the layer
+        - activation is the activation function that should be used
+         on the layer
         - lambtha is the L2 regularization parameter
         Returns: the output of the new layer
     """
