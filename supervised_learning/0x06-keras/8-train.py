d117021ef51143324d6dc4e1c39340c08c6759b0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-02 01:29:36 +0000

    Add exercise 8

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""Save the best iteration of the model"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels,
+                batch_size, epochs,
+                validation_data=None,
+                early_stopping=False,
+                patience=0,
+                learning_rate_decay=False,
+                alpha=0.1,
+                decay_rate=1,
+                save_best=False,
+                filepath=None,
+                verbose=True,
+                shuffle=False):
+    """
+    Save the best iteration of the model
+        - save_best is a boolean indicating whether to save
+         the model after each epoch if it is the best
+        - a model is considered the best if its validation loss
+         is the lowest that the model has obtained
+        - filepath is the file path where the model should be saved
+    """
+
+    callbacks = []
+
+    def learning_rate_decay(epoch):
+        """Calculates the inverse time decay in each epoch"""
+        return alpha / (1 + decay_rate * epoch)
+
+    if validation_data and learning_rate_decay:
+        lr_decay = K.callbacks.LearningRateScheduler(learning_rate_decay,
+                                                     verbose=1)
+        callbacks.append(lr_decay)
+
+    if validation_data and early_stopping:
+        es = K.callbacks.EarlyStopping(monitor='val_loss', patience=patience,
+                                       mode='min')
+        callbacks.append(es)
+
+    if filepath:
+        mcp_save = K.callbacks.ModelCheckpoint(filepath,
+                                               save_best_only=save_best,
+                                               monitor='val_loss',
+                                               mode='min')
+        callbacks.append(mcp_save)
+
+    history = network.fit(x=data, y=labels, batch_size=batch_size,
+                          epochs=epochs, verbose=verbose, shuffle=shuffle,
+                          validation_data=validation_data,
+                          callbacks=callbacks)
+
+    return history
