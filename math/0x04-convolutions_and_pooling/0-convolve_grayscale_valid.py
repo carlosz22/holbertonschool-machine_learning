ea53575aa826955ff00e2e0abd683cba096fabea
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-05 02:43:02 +0000

    FIx PEP8 exer 0

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -8,14 +8,17 @@ from math import ceil, floor
 def convolve_grayscale_valid(images, kernel):
     """
     Performs a valid convolution on grayscale images
-        - images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+        - images is a numpy.ndarray with shape (m, h, w)
+         containing multiple grayscale images
             - m is the number of images
             - h is the height in pixels of the images
             - w is the width in pixels of the images
-        - kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+        - kernel is a numpy.ndarray with shape (kh, kw)
+         containing the kernel for the convolution
             - kh is the height of the kernel
             - kw is the width of the kernel
-        You are only allowed to use two for loops; any other loops of any kind are not allowed
+        You are only allowed to use two for loops; any
+         other loops of any kind are not allowed
         Returns: a numpy.ndarray containing the convolved images
     """
     images_m = images.shape[0]
@@ -35,6 +38,7 @@ def convolve_grayscale_valid(images, kernel):
         for y in range(output_h):
             output[:, y, x] = (kernel * images[:,
                                                y: y + kernel_h,
-                                               x: x + kernel_w]).sum(axis=(1, 2))
+                                               x: x + kernel_w])\
+                                               .sum(axis=(1, 2))
 
     return output
