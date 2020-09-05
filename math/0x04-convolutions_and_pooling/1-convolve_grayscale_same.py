4cc4b0c1feccea34d6deac430251264af9027a66
Carlos Zuluaga <carlosez9@hotmail.com>
2020-09-05 03:20:58 +0000

    Add exercise 1 and fix variables names

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""Performs a valid convolution on grayscale images"""
+"""Performs a same convolution on grayscale images"""
 import numpy as np
 
 
-def convolve_grayscale_valid(images, kernel):
+def convolve_grayscale_same(images, kernel):
     """
-    Performs a valid convolution on grayscale images
+    Performs a same convolution on grayscale images
         - images is a numpy.ndarray with shape (m, h, w)
          containing multiple grayscale images
             - m is the number of images
@@ -16,6 +16,7 @@ def convolve_grayscale_valid(images, kernel):
          containing the kernel for the convolution
             - kh is the height of the kernel
             - kw is the width of the kernel
+        - if necessary, the image should be padded with 0’s
         You are only allowed to use two for loops; any
          other loops of any kind are not allowed
         Returns: a numpy.ndarray containing the convolved images
@@ -27,17 +28,20 @@ def convolve_grayscale_valid(images, kernel):
     kh = kernel.shape[0]
     kw = kernel.shape[1]
 
-    output_h = int(h - kh + 1)
-    output_w = int(iw - kw + 1)
-    output_d = m
+    output = np.zeros((m, h, w))
 
-    output = np.zeros((output_d, output_h, output_w))
+    ph = max(int((kh - 1) / 2), int(kh / 2))
+    pw = max(int((kw - 1) / 2), int(kw / 2))
 
-    for x in range(output_w):
-        for y in range(output_h):
-            output[:, y, x] = (kernel * images[:,
-                                               y: y + kh,
-                                               x: x + kw])\
-                                               .sum(axis=(1, 2))
+    pad_size = ((0, 0), (ph, ph), (pw, pw))
+    pad_images = np.pad(images, pad_width=pad_size, mode='constant',
+                        constant_values=0)
+
+    for x in range(w):
+        for y in range(h):
+            output[:, y, x] = (kernel * pad_images[:,
+                                                   y: y + kh,
+                                                   x: x + kw])\
+                                                   .sum(axis=(1, 2))
 
     return output
